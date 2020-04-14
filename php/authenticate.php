967653743e6417ccb24be20669ca8a837abb0359
NooraSajjad <noora.sajjad@gmail.com>
2020-04-15 00:30:58 +0300

    slight edit

--- php/authenticate.php
+++ php/authenticate.php
@@ -18,7 +18,7 @@ if (!isset($_POST['username'], $_POST['password'])) {
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
 if ($stmt = $con->prepare('SELECT id, password, full_name FROM accounts WHERE username = ?')) {
-    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    // Bind parameters, in our case the username is a string so we use "s"
     $stmt->bind_param('s', $_POST['username']);
     $stmt->execute();
     // Store the result so we can check if the account exists in the database.
